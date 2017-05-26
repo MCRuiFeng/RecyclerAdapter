3de4ea151d1d02eb2d0e2170cc7be4d82bcb6aa4
MCRuiFeng <dev.mcrf@qq.com>
2017-05-27 01:34:41 +0800

    2017.05.27

--- RecyclerAdapter/src/main/java/com/mcrf/eg/recycleradapter/RecyclerAdapter.java
+++ RecyclerAdapter/src/main/java/com/mcrf/eg/recycleradapter/RecyclerAdapter.java
@@ -10,39 +10,46 @@ import com.mcrf.eg.recycleradapter.R;
 import java.util.List;
 
 
-/*
+/* 最普通的RecyclerView适配器，只能显示一个Title
  * By Github MCRuiFeng
  * Email:dev.mcrf@qq.com
  * 2017.05.25
  */
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder> {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder>
+{
 
     private List<String> listDatas;
     private LayoutInflater layoutInflater;
     private OnItemClickListener onItemClickListener;
 
-    public RecyclerAdapter(Context context, List<String> listDatas) {
+    public RecyclerAdapter(Context context, List<String> listDatas)
+	{
         this.listDatas = listDatas;
         layoutInflater = LayoutInflater.from(context);
     }
 
 
     @Override
-    public int getItemCount() {
+    public int getItemCount()
+	{
         return listDatas.size();
     }
 
 
 	@Override
-    public void onBindViewHolder(final ItemViewHolder itemViewHolder, final int i) {
+    public void onBindViewHolder(final ItemViewHolder itemViewHolder, final int i)
+	{
 
 		itemViewHolder.title.setText(listDatas.get(i));
 
-		if (onItemClickListener != null) {
-            if (!itemViewHolder.itemView.hasOnClickListeners()) {
+		if (onItemClickListener != null)
+		{
+            if (!itemViewHolder.itemView.hasOnClickListeners())
+			{
                 itemViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
 						@Override
-						public void onClick(View v) {
+						public void onClick(View v)
+						{
 							int pos = itemViewHolder.getPosition();
 							onItemClickListener.onItemClick(v, pos);
 						}
@@ -53,39 +60,34 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemVi
 
 
     @Override
-    public ItemViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+    public ItemViewHolder onCreateViewHolder(ViewGroup viewGroup, int i)
+	{
         ItemViewHolder holder = new ItemViewHolder(layoutInflater.inflate(R.layout.list, viewGroup, false));
         return holder;
     }
 
 
-	public void add(int position, String value) {
-        if (position > listDatas.size()) {
-            position = listDatas.size();
-        }
-        if (position < 0) {
-            position = 0;
-        }
-        listDatas.add(position, value);
-        notifyItemInserted(position);
-    }
-
-
-    public void setOnItemClickListener(OnItemClickListener mOnItemClickListener) {
+    public void setOnItemClickListener(OnItemClickListener mOnItemClickListener)
+	{
         this.onItemClickListener = mOnItemClickListener;
     }
 
 
-	interface OnItemClickListener {
+	interface OnItemClickListener
+	{
         public void onItemClick(View view, int position);
     }
 
 
-    class ItemViewHolder extends RecyclerView.ViewHolder {
+
+	
+    class ItemViewHolder extends RecyclerView.ViewHolder
+	{
 
         private TextView title;
 
-        public ItemViewHolder(View itemView) {
+        public ItemViewHolder(View itemView)
+		{
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.item_tv);
         }
